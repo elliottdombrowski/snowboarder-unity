cfab2ed101545a0a2491552f0b6a93ddb6d41c35
elliott dombrowski <elliottroyal@me.com>
2023-01-02 12:53:27 -0600

    scene load

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.tag == "Ground")
     {
-        
+      Debug.Log("Hit the ground");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 }
