d7eea138300fea46821d5f5b8dbc2465f353808c
elliott dombrowski <elliottroyal@me.com>
2023-01-02 16:54:40 -0600

    boost speed functionalist

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] float torqueAmount = 1f;
+  [SerializeField] float boostSpeed = 30f;
+  [SerializeField] float baseSpeed = 20f;
   Rigidbody2D rb2d;
+  SurfaceEffector2D surfaceEffector2D;
   void Start()
   {
     rb2d = GetComponent<Rigidbody2D>();
+    surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
   }
 
-  // Update is called once per frame
   void Update()
+  {
+    RotatePlayer();
+    RespondToBoost();
+  }
+
+  void RotatePlayer()
   {
     if (Input.GetKey(KeyCode.LeftArrow))
     {
@@ -21,6 +30,20 @@ public class PlayerController : MonoBehaviour
     else if (Input.GetKey(KeyCode.RightArrow))
     {
       rb2d.AddTorque(-torqueAmount);
+    }
+  }
+
+  void RespondToBoost()
+  {
+    // IF PLAYER PRESSES UP ARROW, SPEED UP
+    // OTHERWISE STAY SAME SPEED
+    if(Input.GetKey(KeyCode.UpArrow))
+    {
+      surfaceEffector2D.speed = boostSpeed;
+    }
+    else 
+    {
+      surfaceEffector2D.speed = baseSpeed;
     }
   }
 }
