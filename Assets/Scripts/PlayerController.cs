5ad63134409c51cb7e682ed6767137368e0eb2e3
elliott dombrowski <elliottroyal@me.com>
2023-01-02 12:06:11 -0600

    tweak angular drag

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] float torqueAmount = 1f;
-  RigidBody2D rb2d;
+  Rigidbody2D rb2d;
   void Start()
   {
-    rb2d = GetComponent<RigidBody2D>();
+    rb2d = GetComponent<Rigidbody2D>();
   }
 
   // Update is called once per frame
   void Update()
   {
-    if(Input.GetKey(KeyCode.LeftArrow))
+    if (Input.GetKey(KeyCode.LeftArrow))
     {
       rb2d.AddTorque(torqueAmount);
+    }
+    else if (Input.GetKey(KeyCode.RightArrow))
+    {
+      rb2d.AddTorque(-torqueAmount);
     }
   }
 }
