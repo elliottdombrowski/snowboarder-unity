03d6d872b3427af5db7c32093beb0a8ff6a5440b
elliott dombrowski <elliottroyal@me.com>
2023-01-02 18:41:29 -0600

    prevent doubleplay on crash audio

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,10 +8,12 @@ public class CrashDetector : MonoBehaviour
   [SerializeField] float loadDelay = 0.5f;
   [SerializeField] ParticleSystem crashEffect;
   [SerializeField] AudioClip crashSFX;
+  bool hasCrashed = false;
   void OnTriggerEnter2D(Collider2D other)
   {
-    if(other.tag == "Ground")
+    if(other.tag == "Ground" && !hasCrashed)
     {
+      hasCrashed = true;
       FindObjectOfType<PlayerController>().DisableControls(); // DISABLE CONTROLS VIA METHOD IN PLAYERCONTROLLER.CS
       crashEffect.Play();
       GetComponent<AudioSource>().PlayOneShot(crashSFX);
